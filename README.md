0f3c5543204f850b599fd568c44a9ff136771081
Aksoylu <umit@aksoylu.space>
2022-02-06 14:27:19 +0300

    SQL migration file added & Readme updated

--- README.md
+++ README.md
@@ -8,5 +8,19 @@ An URL Shortening Service Created with Proton Framework <br>
 <br>
 <a href="http://proton.aksoylu.space">Click for Proton Framework</a>
 
-## How it works ?
-Coming soon...
+## How to install & staring up the project ?
+1) Import the '/urlvector__migrate.sql' file on your MySQL (or MariaDb) server
+2) Specify the database credentials in '/config.php'. (Also give attention for your database's true charset format, mine was utf-8)
+3) Also you have to configure "PROJECT_NAME" and "DOMAIN" fields for your conditions.
+4) If you are using SSL certificate, you have to change "PROTOCOL" to "https" from "http"
+5) Don't touch anything other than these.
+
+Note that this is an open source project with GPL licence soi  don't guarantee your security. You have to pay attention on your own server security.
+
+## Usage (DEMO)
+
+You can check a running demo on <a href="http://url.aksoylu.space">URL.AKSOYLU.SPACE</a> (My own Url shortener service)
+
+<center>
+<img src="http://proton.aksoylu.space/view/assets/img/demo.gif" alt="Url Vector Demo" width="80%"/>
+</center>
---
+++ urlvector__migrate.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 06 Şub 2022, 12:19:47
+-- Sunucu sürümü: 10.4.17-MariaDB
+-- PHP Sürümü: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `urlvector`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `urls`
+--
+
+CREATE TABLE `urls` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `issuing_date` datetime NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `navigation_url` varchar(128) NOT NULL,
+  `navigation_delay` int(11) NOT NULL,
+  `navigation_text` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `urls`
+--
+
+INSERT INTO `urls` (`id`, `name`, `issuing_date`, `password`, `navigation_url`, `navigation_delay`, `navigation_text`) VALUES
+(1, 'aksoylu', '2022-02-06 10:58:20', '4c56ff4ce4aaf9573aa5dff913df997a', 'http://umit.space', 3, 'Ümit Aksoylu\'s Personal Website and Blog'),
+(2, 'google', '2022-02-06 10:58:20', '4c56ff4ce4aaf9573aa5dff913df997a', 'http://google.com', 0, '');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `urls`
+--
+ALTER TABLE `urls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `urls`
+--
+ALTER TABLE `urls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
